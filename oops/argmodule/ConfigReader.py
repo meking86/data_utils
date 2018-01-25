d0d5529282752f0ec7c5af4933cd39497020073f
keswaramoorthy <keswaramoorthy@apple.com>
2018-01-24 16:38:00 -0800

    Initial commit

---
+++ oops/argmodule/ConfigReader.py
@@ -0,0 +1,14 @@
+class ConfigReader:
+
+   def __init__(self):
+       print('I am Getting initialized : ConfigReaderClass')
+
+   def readerINI(self):
+       print('I am reader for INI File')
+
+   def readerYAML(self):
+       print('I am reader for YAML File')
+
+   def readerJSON(self):
+       print('I am reader for JSON File')
+   
---
+++ oops/getproperties.py
@@ -0,0 +1,26 @@
+from argmodule.ConfigReader  import ConfigReader
+import importlib
+import argparse
+
+parser = argparse.ArgumentParser("description='Used to get configuration file path and configuration file type'")
+parser.add_argument('--type','-t',required=True,help='Use to get configuration file type Need Type',dest='config_type')
+parser.add_argument('--path','-p',required=True,help='Use to get configutaion file path',dest='config_path')
+results = parser.parse_args()
+
+print(results.config_path,results.config_type)
+
+#configins = ConfigReader()
+module = importlib.import_module('argmodule.ConfigReader')
+my_class= getattr(module, 'ConfigReader')
+my_instance = my_class()
+
+func = getattr(my_instance,'readerJSON','fun_when_not_find') 
+func() 
+
+def fun_when_not_find(self):
+    print('Please provide a valid Type file to read ')
+#def is_valid_type(parser,x):
+#    lis = ['YAML','INI','JSON']
+#    if (x in lis):
+#        return(x)
+
